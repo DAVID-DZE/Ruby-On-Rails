201a93d2f31a96fad2e8c27f94065a13fcebf303
David <david.dzeveckij@samesystem.com>
2024-05-28 15:58:44 +0300

    linting and rubocop

--- 1_Ruby/Projects/linting/caesar_cipher.rb
+++ 1_Ruby/Projects/linting/caesar_cipher.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 def caesar_cipher(str, shift)
-  str.split('').map do |c|
+  str.chars.map do |c|
     ascii = c.ord
     if ascii.between?('a'.ord, 'z'.ord)
-      ((ascii - 'a'.ord + shift) % 26 + 'a'.ord).chr
+      (((ascii - 'a'.ord + shift) % 26) + 'a'.ord).chr
     elsif ascii.between?('A'.ord, 'Z'.ord)
-      ((ascii - 'A'.ord + shift) % 26 + 'A'.ord).chr
+      (((ascii - 'A'.ord + shift) % 26) + 'A'.ord).chr
     else
       c
     end
